8247931407945f5d24a7f80631dd1882d53d2bae
ckpritha <84943720+ckpritha@users.noreply.github.com>
2021-07-13 09:19:59 -0400

    comment added

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -6,7 +6,7 @@ public class test {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
 		System.out.println("connection successful");
-		
+		//jenkins connection successful
 	}
 
 }
